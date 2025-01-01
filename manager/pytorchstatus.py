2f8b69edc3d2998fd305d56f148791d68c827b0a
Kavya <soldatargent@gmail.com>
2025-01-01 12:09:37 +0530

    Use event time to filter old events

--- manager/pytorchstatus.py
+++ manager/pytorchstatus.py
@@ -1,38 +1,49 @@
 from kubernetes import client, config,watch
-from datetime import date, datetime, time, timedelta
+from datetime import date, datetime, time, timedelta, timezone
 import time
 import sys
 import requests
 import json
 from manager import scale
 
-def getpytorchjobstatus(interval):
+def load_config():
     try:
         config.load_kube_config('kubeconfig')
     except Exception as e:
         config.load_incluster_config()
 
+def update_job(jobname):
+        jobstatus = {"job_name": jobname, "status": "completed"}
+        headers = {"Content-Type": "application/json"}
+        def_url = "http://job-metadata-manager-service.fms-tuning.svc.cluster.local:5000/update_job_status"
+        url = sys.argv[2] if len(sys.argv) >= 2 else def_url
+        resp = requests.put(url, json=jobstatus, headers=headers)
+        if resp.status_code != 200:
+            print("Updating job status failed: ", resp.json())
+        else:
+            print("Updated job status")
+
+def getpytorchjobstatus(interval=10):
+    load_config()
     v1 = client.CoreV1Api()
     field_selector='reason=PyTorchJobSucceeded'
-    #default time diff 10 minutes
+
     time_diff=int(sys.argv[1]) if len(sys.argv) > 1 else interval
     t_sent = datetime.now() - timedelta(minutes = time_diff)
+
     for event in watch.Watch().stream(v1.list_namespaced_event, "fms-tuning", field_selector=field_selector,watch=True):
-        print(event['object'].message)
-        t_now = datetime.now()
-        curr_diff = t_now - t_sent
-        if(curr_diff > timedelta(minutes=time_diff)):
-                print("send to workload mgr")
-                print(event['object'].message)
+        t_event = print(event['object']['metadata']['creation_timestamp'])
+        t_now = datetime.now(timezone.utc)
+        curr_diff = t_now - t_event
+        print(f'{event['object'].message} with time diff {curr_diff}')
+        if(curr_diff < timedelta(minutes=time_diff)):
                 jobname = event['object'].involved_object.name
-                jobstatus = {"job_name": jobname, "status": "completed"}
-                headers = {"Content-Type": "application/json"}
-                def_url = "http://job-metadata-manager-service.fms-tuning.svc.cluster.local:5000/update_job_status"
-                url = sys.argv[2] if len(sys.argv) >= 2 else def_url
-                response = requests.post(url, data=json.dumps(jobstatus), headers=headers)
-                t_sent = t_now
-                time_diff = 1
+                update_job(jobname)
+                print("Calling scale")
                 scale()
+        else:
+            print("Event too old, not calling manager")
+
 #donot send event notification older than `tp` minutes             
 tp = 10
 getpytorchjobstatus(tp)
