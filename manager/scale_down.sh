b2d2e6bcaa7a5f2572f8142073ec28217d42d6b3
Seep Goel <seep.goyal@gmail.com>
2025-01-06 12:00:22 +0530

    extended the demo script to include scale down

---
+++ manager/scale_down.sh
@@ -0,0 +1,17 @@
+echo "Starting 1st Job"
+cp ../monitoring/sample_json/outputj1.json ../monitoring/sample_json/output.json
+./main.py deploy -f yamls/demo-job1.yaml
+sleep $1
+echo "Starting 2nd Job"
+cp ../monitoring/sample_json/outputj2.json ../monitoring/sample_json/output.json
+./main.py deploy -f yamls/demo-job2.yaml
+sleep $2
+echo "Starting 3rd Job"
+cp ../monitoring/sample_json/outputj3.json ../monitoring/sample_json/output.json
+./main.py deploy -f yamls/demo-job3.yaml
+#for scale up
+cp ../monitoring/sample_json/outputsup.json ../monitoring/sample_json/output.json
+#for scale down
+sleep $3
+cp ../monitoring/sample_json/outputsdown.json ../monitoring/sample_json/output.json
+./main.py deploy -f yamls/demo-job4.yaml
