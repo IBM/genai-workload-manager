4bab986d71c4ff77faeb26b1c554d088132081fa
Kavya <soldatargent@gmail.com>
2025-01-09 15:33:18 +0530

    Starter script renamed, fixed for scale down, pretty print

--- manager/orchestrator.sh
+++ manager/orchestrator.sh
@@ -2,10 +2,12 @@ echo "Starting 1st Job"
 cp ../monitoring/sample_json/outputj1.json ../monitoring/sample_json/output.json
 ./main.py deploy -f yamls/demo-job1.yaml
 sleep $1
+echo "============================================================"
 echo "Starting 2nd Job"
 cp ../monitoring/sample_json/outputj2.json ../monitoring/sample_json/output.json
 ./main.py deploy -f yamls/demo-job2.yaml
 sleep $2
+echo "============================================================"
 echo "Starting 3rd Job"
 cp ../monitoring/sample_json/outputj3.json ../monitoring/sample_json/output.json
 ./main.py deploy -f yamls/demo-job3.yaml
@@ -13,5 +15,8 @@ cp ../monitoring/sample_json/outputj3.json ../monitoring/sample_json/output.json
 cp ../monitoring/sample_json/outputsup.json ../monitoring/sample_json/output.json
 #for scale down
 sleep $3
+echo "============================================================"
+echo "Starting 4th Job"
 cp ../monitoring/sample_json/outputsdown.json ../monitoring/sample_json/output.json
+{ sleep 20; cp ../monitoring/sample_json/outputsdown2.json ../monitoring/sample_json/output.json; } &
 ./main.py deploy -f yamls/demo-job4.yaml
